dec33e1c00da87ea65e0fb27e9c4062c5be5f42b
Echlas Malik <99184338+EchlasMalik@users.noreply.github.com>
2024-08-02 19:37:29 +0100

    Added Simple Solidity Storage Contract

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+//Compiles to EVM - The Ethereum Virtual Machine
+
+contract SimpleStorage {
+    // boolean, unint, int, address, bytes
+
+    //This gets initialised to 0
+    uint256 favouriteNumber;
+
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    // uint256[] public favouriteNumbersList;
+    People[] public people;
+    
+    
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber;
+        retrieve();
+    }
+
+    // view, pure
+    function retrieve() public view returns(uint256){
+        return favouriteNumber;
+    }
+
+    // Adds to the mapping AND the Array
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(People(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
